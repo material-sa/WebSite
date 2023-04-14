35aa56537c28e9376d115d6c46e5134d50e15e00
nourahassan <nourahassan279@gmail.com>
2023-04-14 17:24:24 +0200

    iniatized pages

--- components/home/homeBanner.vue
+++ components/home/homeBanner.vue
@@ -1,14 +1,13 @@
 <template>
   <section class="section meet-doctors bg-white">
-	 <div id="our_doctor" class="owl-carousel text-center">
-<client-only>
-   <carousel v-bind="options">
+   <carousel v-bind="options" >
+	<slide v-for="item in categories" :key="item.id">
       <div class="item">
         <div class="doctor text-center">
           <a href="doctor-details.html">
             <img
-              src="~/assets/img/Pepsi.png"
-              alt="Dr. Cristina Groves"
+              :src="item.img"
+              :alt="item. title"
               class="rounded-circle"
               width="150"
               height="150"
@@ -17,95 +16,42 @@
           </a>
         </div>
       </div>
-      <div class="item">
-        <div class="doctor text-center">
-          <a href="doctor-details.html">
-            <img
-              src="~/assets/img/coca-cola.png"
-              alt="Dr. Henry Daniels"
-              class="rounded-circle"
-              width="150"
-              height="150"
-            />
-            <div class="doctors-name1">Coca-Cola</div>
-          </a>
-        </div>
-      </div>
-      <div class="item">
-        <div class="doctor text-center">
-          <a href="doctor-details.html">
-            <img
-              src="~/assets/img/coffe.png"
-              alt="Dr. Diana Bailey"
-              class="rounded-circle"
-              width="150"
-              height="150"
-            />
-            <div class="doctors-name1">Coffee</div>
-          </a>
-        </div>
-      </div>
-      <div class="item">
-        <div class="doctor text-center">
-          <a href="doctor-details.html">
-            <img
-              src="~/assets/img/soft.png"
-              alt="Dr. Justin Parker"
-              class="rounded-circle"
-              width="150"
-              height="150"
-            />
-            <div class="doctors-name1">Soft Drinks</div>
-          </a>
-        </div>
-      </div>
-      <div class="item">
-        <div class="doctor text-center">
-          <a href="doctor-details.html">
-            <img
-              src="~/assets/img/Beverge.png"
-              alt="Dr. Marie Wells"
-              class="rounded-circle"
-              width="150"
-              height="150"
-            />
-            <div class="doctors-name1">Beverages</div>
-          </a>
-        </div>
-      </div>
-      <div class="item">
-        <div class="doctor text-center">
-          <a href="doctor-details.html">
-            <img
-              src="~/assets/img/coffe.png"
-              alt="Dr. Pamela Curtis"
-              class="rounded-circle"
-              width="150"
-              height="150"
-            />
-            <div class="doctors-name1">Coffee</div>
-          </a>
-        </div>
-      </div>
-      <div class="item">
-        <div class="doctor text-center">
-          <a href="doctor-details.html">
-            <img
-              src="~/assets/img/Nova.png"
-              alt="Dr. Ronald Jacobs"
-              class="rounded-circle"
-              width="150"
-              height="150"
-            />
-            <div class="doctors-name1">Nova</div>
-          </a>
-        </div>
-      </div>
+	  </slide>
+	 
 	  </carousel>
-        </client-only>
-    </div>
   </section>
 </template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      options: {
+			perPage:5,
+			navigationEnabled:false,
+			paginationEnabled:true,
+			autoplayHoverPause:true,
+			autoplayTimeout:1000,
+			autoplay:true
+      },categories:[
+		{"id":1,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":2,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":3,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":4,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":5,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":6,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":7,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":8,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":9,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+		{"id":10,"img":"~/assets/img/water.png" ,"title":"Dr. Albert Sandoval"},
+
+		
+	  ]
+    }
+  },
+}
+</script>
 
 
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+ <div class="main-content">
     <HomeSlider />
     <HomeBanner />
     <ProductSlider :title="$t('main_page.new_products')" />
