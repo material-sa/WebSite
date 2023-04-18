6584be0681e7d3da13e9bdf2b786cdd657786423
nourahassan <nourahassan279@gmail.com>
2023-04-19 00:06:26 +0200

    translate pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
 <template>
- <div class="main-content">
+  <div class="main-content">
     <HomeSlider />
-    <HomeBanner />
-    <ProductSlider :title="$t('main_page.new_products')" />
-    <ProductSlider :title="$t('main_page.best_sellers')" />
-    <ProductSlider :title="$t('main_page.suggested_products')" />
+    <HomeBanner :categories="categories" @filtered="filterProducts" />
+    <ProductSlider
+      :title="$t('main_page.suggested_products')"
+      :products="suggested_products"
+      :type="'suggested'"
+    />
+    <ProductSlider
+      :title="$t('main_page.new_products')"
+      :products="new_products"
+    />
+    <ProductSlider
+      :title="$t('main_page.best_sellers')"
+      :products="best_sellers"
+    />
   </div>
 </template>
 
@@ -13,15 +23,45 @@
 import HomeSlider from '@/components/home/slider.vue'
 import HomeBanner from '@/components/home/homeBanner.vue'
 import ProductSlider from '@/components/home/productSlider.vue'
+import HOME_API from '@/services/apis/home_api'
 
 export default {
   components: { HomeSlider, ProductSlider, HomeBanner },
-  created(){
-	//this.mymethod();
-	 this.$axios.get('product/dynamic-home-listing').then(res=>{
-	 	console.log("result : ", res.data)
-	 })
-  }
+  data() {
+    return {
+      categories: [],
+      new_products: [],
+      best_sellers: [],
+      suggested_products: [],
+    }
+  },
+  methods: {
+    async getData() {
+      const all_data = await HOME_API.gethomeData()
+      this.categories = all_data.responseData.categories
+      this.suggested_products = this.categories[0].products
+      this.new_products = all_data.responseData.newlyArrivedProducts
+      this.best_sellers = all_data.responseData.bestSellerProducts
+    },
+    filterProducts(e) {
+      this.suggested_products = e
+    },
+  },
+  created() {
+    //this.mymethod();
+    this.getData()
+
+    //setting locale
+    process.client ? localStorage.setItem('locale', this.$i18n.locale) : ''
+  },
+  watch: {
+    '$i18n.locale': {
+      handler: function (val) {
+        this.getData()
+      },
+      deep: true
+    },
+  },
 }
 </script>
 
