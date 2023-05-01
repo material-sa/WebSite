f81a52e62b410100e83e586834f42049fe8d322e
material-sa <100345510+material-sa@users.noreply.github.com>
2023-05-02 00:52:57 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -123,7 +123,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://staging.materiel.sa/api/v3/',
+    baseURL: 'https://app.material.sa/api/v3/',
     //prefix: '/api',
     credentials: true
   },
