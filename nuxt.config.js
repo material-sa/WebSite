da9161e276798b7f025a316006fef3d6ebf39dd8
material-sa <100345510+material-sa@users.noreply.github.com>
2023-05-02 15:02:53 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+      'bootstrap-vue/nuxt'
+
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
