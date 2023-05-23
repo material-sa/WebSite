1377ad089220c2658650f0082af0d95511eaee0d
material-sa <100345510+material-sa@users.noreply.github.com>
2023-05-23 14:44:34 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      {name:'google-site-verification', content:'pP3gilFAJXRkTCnRwZjSmm7RFgYyiaFPHEVpK_qgYMo'},
     ],
     link: [{ rel: 'icon', type: 'image/png', href: '/icon.png' }],
     script: [
